60bc2a488923e38553924ef69b454aed8848901e
yangxiaomei <yangxiaomei@ybm100.com>
2019-07-31 18:47:22 +0800

    增加登录

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const controller = require('./controller.js');
 
 app.use(async (ctx,next)=>{
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`)
-    await next();
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    if (ctx.method == 'OPTIONS') {
+      ctx.body = 200; 
+    } else {
+      await next();
+    }
 })
 
 app.use(bodyParser())
--- controllers/login.js
+++ controllers/login.js
@@ -2,10 +2,11 @@
 const fn_login = async (ctx,next)=>{
     const name = ctx.request.body.name || '';
     const pwd = ctx.request.body.password || '';
-    const data = {code:0,msg:'',result:`hello!${name},登录成功`}
+    const data = {code:0,massage:'',result:`hello!${name},登录成功`}
     if(name!=='logmei'&&pwd!=='123456'){
         data.code=1
-        data.msg='登录失败'
+        data.massage='登录失败'
+        data.result = ''
     }
     ctx.response.body = JSON.stringify(data)
 }
